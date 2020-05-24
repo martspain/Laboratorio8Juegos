784f8a6c06e0abbadd7e4bcb6b24beddf4c253a5
Martín España <martineduardoer00@gmail.com>
2020-05-24 04:02:39 -0600

    Pequeños cambios: cuarto con objetos con nombre terminado

---
+++ Lab 8/Assets/Scripts/HousePropBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HousePropBehaviour : MonoBehaviour
+{
+
+    public GameObject text;
+
+    private void OnMouseEnter()
+    {
+        if (text) 
+        {
+            text.GetComponent<Text>().text = gameObject.tag;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (text)
+        {
+            text.GetComponent<Text>().text = "";
+        }
+    }
+
+}
---
+++ Lab 8/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject instructions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instructions)
+            instructions.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (instructions)
+            instructions.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (instructions)
+            instructions.SetActive(false);
+    }
+}
